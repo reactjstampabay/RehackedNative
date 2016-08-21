e943130f12681b66e7476aa8c5d3a865b24f911f
johnrhampton <johnrhampton@gmail.com>
2016-08-21 16:17:51 -0400

    adding NavigatorIOS to iOS entry point

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import {NavigatorIOS, AppRegistry} from 'react-native';
+import StartScreen from './src/containers/StartScreen';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-class RehackedNative extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+export default RehackedNative = () => {
+  return (
+    <NavigatorIOS
+      initialRoute={{
+        component: StartScreen,
+        title: 'ReHacked Native Title!'
+      }}
+      navigationBarHidden={false}
+      style={{flex: 1}}
+    />
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('RehackedNative', () => RehackedNative);
