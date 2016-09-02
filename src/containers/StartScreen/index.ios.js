0ede7a5004da00fdaa2213c43eeb97f580cec8ff
johnrhampton <johnrhampton@gmail.com>
2016-09-02 14:32:44 -0400

    StartScreen returns the Login component

--- src/containers/StartScreen/index.ios.js
+++ src/containers/StartScreen/index.ios.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import Login from './Login';
 
 export default () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>
-        Welcome to ReHacked Native iOS!
-      </Text>
-    </View>
+    <Login/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
-});
