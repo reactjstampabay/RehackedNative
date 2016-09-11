225786390f372a189b8342a454177644cc5b129a
johnrhampton <johnrhampton@gmail.com>
2016-09-11 10:30:56 -0400

    Provider HOC, import rehacked-common, create Redux store

--- index.ios.js
+++ index.ios.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import {NavigatorIOS, AppRegistry} from 'react-native';
+import {Provider} from 'react-redux';
+import createStore from 'rehacked-common/lib/store';
+
 import StartScreen from './src/containers/StartScreen';
+
+/**
+ * create Redux store
+ */
+const store = createStore();
 
 export default RehackedNative = () => {
   return (
-    <NavigatorIOS
-      initialRoute={{
-        component: StartScreen,
-        title: 'ReHacked Native Title!'
-      }}
-      navigationBarHidden={true}
-      style={{flex: 1}}
-    />
+    <Provider store={store}>
+      <NavigatorIOS
+        initialRoute={{
+          component: StartScreen,
+          title: 'ReHacked Native Title!'
+        }}
+        navigationBarHidden={true}
+        style={{flex: 1}}
+      />
+    </Provider>
   );
 }
 
